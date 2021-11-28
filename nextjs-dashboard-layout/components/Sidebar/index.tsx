589ff0f859ceeb7307ff5aeac1420f3e758cceeb
Katsiaryna Lupachova <katerinalupacheva@gmail.com>
2021-11-28 21:24:54 +0300

    Menu options created

--- nextjs-dashboard-layout/components/Sidebar/index.tsx
+++ nextjs-dashboard-layout/components/Sidebar/index.tsx
@@ -1,9 +1,15 @@
 import { SidebarContainer } from "./styles";
+import { MENU_ITEMS } from "../../constants/menu-items";
+import MenuItemsList from "../MenuItemsList";
 
 type SidebarProps = {
   isOpened: boolean;
 };
 
 export default function Sidebar({ isOpened }: SidebarProps) {
-  return <SidebarContainer isOpened={isOpened}>Menu</SidebarContainer>;
+  return (
+    <SidebarContainer isOpened={isOpened}>
+      <MenuItemsList options={MENU_ITEMS} />
+    </SidebarContainer>
+  );
 }
